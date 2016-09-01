9847e3e9991c181b12eb946fbf9dce1c750c61b4
orenjiakira <orenjiakira@gmail.com>
2016-09-01 00:09:05 -0300

    Add resource folder

---
+++ src/resources/smol_slime.lua
@@ -0,0 +1,33 @@
+
+local res = {}
+
+res.img = love.graphics.newImage 'assets/smol-slime.png'
+res.width = res.img:getWidth()
+res.height = res.img:getHeight()
+res.offset = UTILITY.Vector:new{16, 32}
+res.animations = {
+    {
+        name     = "idle",
+        quads    = {
+            love.graphics.newQuad(0*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(1*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(2*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(3*32, 0, 32, 48, res.img:getDimensions()),
+        },
+        step     = 0.2,
+        loop     = true,
+    },
+    {
+        name     = "moving",
+        quads    = {
+            love.graphics.newQuad(0*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(1*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(2*32, 0, 32, 48, res.img:getDimensions()),
+            love.graphics.newQuad(3*32, 0, 32, 48, res.img:getDimensions()),
+        },
+        step     = 0.1,
+        loop     = true,
+    }
+}
+
+return res
