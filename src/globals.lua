3f87c039205ab1ca4e22d398f9a7bdc539a0dc49
orenjiakira <orenjiakira@gmail.com>
2016-09-01 00:08:13 -0300

    Fix global values

--- src/globals.lua
+++ src/globals.lua
@@ -4,7 +4,8 @@ local globals = {}
 globals.framerate = 60
 globals.frameunit = 1/globals.framerate
 globals.unit = 32
-globals.game_width, globals.game_weight = love.graphics.getDimensions()/globals.unit
+globals.game_width, globals.game_height = love.graphics.getDimensions()
+globals.game_width, globals.game_height = globals.game_width/globals.unit, globals.game_height/globals.unit
 globals.null_function = function() return end
 
 return globals
